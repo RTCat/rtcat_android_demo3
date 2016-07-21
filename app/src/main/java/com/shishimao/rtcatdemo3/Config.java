a6497fae36791357c4379dbe6e868faf37f38ee7
spacetime <jingweiming-sp@outlook.com>
2016-07-21 15:06:39 +0800

    sdk update

--- app/src/main/java/com/shishimao/rtcatdemo3/Config.java
+++ app/src/main/java/com/shishimao/rtcatdemo3/Config.java
@@ -5,7 +5,8 @@ package com.shishimao.rtcatdemo3;
  * Created by chencong on 7/11/16.
  */
 public class Config{
-    public final static String APIKEY = "91af9be9-7bac-40df-962d-f141eebe9d43";
-    public final static String SECRET = "ce8df259-1f7a-4aa4-9e4e-862af9f6c24d";
-    public final static String SESSION = "073df4a9-e9ef-4f45-a2b0-e14c778bcd86";
+    //TODO
+    public final static String APIKEY = "";
+    public final static String SECRET = "";
+    public final static String SESSION = "";
 }
